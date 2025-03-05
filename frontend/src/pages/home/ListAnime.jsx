3bed59de892a9e01de165d3e929b6f9a4dd95fad
jameskiongo <kiongojames2000@gmail.com>
2025-03-05 10:44:52 +0300

    added pagination to current airing section

--- frontend/src/pages/home/ListAnime.jsx
+++ frontend/src/pages/home/ListAnime.jsx
@@ -13,6 +13,13 @@ function ListAnime() {
     page: currentPage,
     limit: itemsPerPage,
   });
+
+  const handleNextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
   let content;
 
   if (isLoading || isFetching) {
@@ -27,8 +34,8 @@ function ListAnime() {
     });
   }
   return (
-    <div className="p-4 z-30">
-      <div className="py-2">
+    <div className="p-4 ">
+      <div className="py-2 flex flex-row justify-between items-center">
         <div className="flex flex-row items-center justify-start">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -59,6 +66,63 @@ function ListAnime() {
             />
           </svg>
           <h1 className="uppercase font-bold px-1">Currently Airing</h1>
+        </div>
+        <div>
+          <nav
+            className="flex items-center -space-x-px"
+            aria-label="Pagination"
+          >
+            <button
+              type="button"
+              disabled={currentPage === 1 || isFetching}
+              className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+              aria-label="Previous"
+              onClick={handlePreviousPage}
+            >
+              <svg
+                className="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m15 18-6-6 6-6"></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              className="min-h-9.5 min-w-9.5 flex justify-center items-center cursor-default bg-[#e9ecef] border border-gray-200 text-gray-800 hover:bg-gray-100 py-2 px-3 text-sm first:rounded-s-lg last:rounded-e-lg focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+            >
+              {currentPage}
+            </button>
+            <button
+              type="button"
+              className="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+              aria-label="Next"
+              onClick={handleNextPage}
+              disabled={!items?.pagination?.has_next_page || isFetching}
+            >
+              <svg
+                className="shrink-0 size-3.5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m9 18 6-6-6-6"></path>
+              </svg>
+            </button>
+          </nav>
         </div>
       </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4 h-40">
