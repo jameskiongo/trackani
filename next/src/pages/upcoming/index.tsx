cbbb88cae3eb08609c0c9b6cccd67ef6060a26aa
jameskiongo <kiongojames2000@gmail.com>
2026-02-15 15:01:51 +0300

    added upcoming anime

--- next/src/pages/upcoming/index.tsx
+++ next/src/pages/upcoming/index.tsx
@@ -35,7 +35,11 @@ export default function UpcomingAnime() {
   let content: ReactNode;
 
   if (isLoading) {
-    content = <div>Loading...</div>;
+    content = (
+      <div className="w-full bg-white min-h-screen flex items-center justify-center">
+        <div className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-blue-600 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
+      </div>
+    );
   } else if (error) {
     content = <div>Something went wrong</div>;
   } else if (!data?.data || data.data.length === 0) {
